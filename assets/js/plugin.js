bc43e7d619354500c147efd56d93ab3ad72341f1
mohamed emad <mmomda.2010@gmail.com>
2022-06-22 01:12:14 +0200

    second

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -1,17 +1,16 @@
 /********************************
- * 
+ *
  * lazy load  *
- * 
+ *
  ********************************/
 
-
 function lazyLoad() {
-    const images = document.querySelectorAll('.lazy-omd');
+    const images = document.querySelectorAll(".lazy-omd");
 
     const optionsLazyLoad = {
         //  rootMargin: '-50px',
         // threshold: 1
-    }
+    };
 
     const imageObserver = new IntersectionObserver(function (enteries) {
         enteries.forEach(function (entery) {
@@ -22,31 +21,28 @@ function lazyLoad() {
                 imageObserver.unobserve(entery.target);
             }
         });
-
     }, optionsLazyLoad);
 
     images.forEach(function (image) {
-        imageObserver.observe(image)
+        imageObserver.observe(image);
     });
 }
 
 function preloadImage(img) {
-    img.src = img.getAttribute('data-src');
+    img.src = img.getAttribute("data-src");
     img.onload = function () {
-        img.parentElement.classList.remove('loading-omd');
+        img.parentElement.classList.remove("loading-omd");
         img.parentElement.classList.add("loaded-omd");
         img.parentElement.parentElement.classList.add("lazy-head-om");
-    }
+    };
 }
 
 jQuery(document).ready(function ($) {
-
-
-    /******************************** 
-   * 
-   *-slider-om  *
-   * 
-   ********************************/
+    /********************************
+     *
+     *-slider-om  *
+     *
+     ********************************/
     new Swiper(".home_our_branches_ .swiper-container", {
         spaceBetween: 16,
         slidesPerView: 3,
@@ -83,11 +79,11 @@ jQuery(document).ready(function ($) {
         },
     });
 
-    /******************************** 
-   * 
-   *-slider-om  *
-   * 
-   ********************************/
+    /********************************
+     *
+     *-slider-om  *
+     *
+     ********************************/
     new Swiper(".home_how_app_work__ .swiper-container", {
         spaceBetween: 16,
         slidesPerView: 3,
@@ -128,13 +124,11 @@ jQuery(document).ready(function ($) {
         },
     });
 
- 
-
     /********************************
-   * 
-   * tabs block for main singin form  *
-   * 
-   ********************************/
+     *
+     * tabs block for main singin form  *
+     *
+     ********************************/
 
     $(".ss-activate-tab-om").on("click", function (e) {
         e.preventDefault();
@@ -143,25 +137,19 @@ jQuery(document).ready(function ($) {
 
         $(target_id).siblings().removeClass("active-om").hide();
         $(target_id).fadeIn().addClass("active-om");
-
     });
 
-
-
     /********************************
-   * 
-   * show password  *
-   * 
-  ********************************/
+     *
+     * show password  *
+     *
+     ********************************/
     $(".show-password-button-om").on("click", function (e) {
         e.preventDefault();
 
-
         if ($(this).parent().find("input").attr("type") == "text") {
-
             $(this).parent().find("input").attr("type", "password");
             $(this).removeClass("show-om");
-
         } else {
             $(this).parent().find("input").attr("type", "text");
             $(this).addClass("show-om");
@@ -169,39 +157,44 @@ jQuery(document).ready(function ($) {
     });
 
     /********************************
-     * 
+     *
      * footer colapsed in small sizes *
-     * 
+     *
      ********************************/
     //
     if ($(window).width() <= 991) {
-        $('.collapse-head-om').on('click', function (e) {
+        $(".collapse-head-om").on("click", function (e) {
             e.preventDefault();
 
-            $('.collapse-head-om').not(this).parent().find('.list-collapse-om').slideUp();
-            $(this).parent().find('.list-collapse-om').slideToggle({
-                queue: false,
-                complete: function () {
-                    $(".list-collapse-om").each(function () {
-                        if ($(this).css("display") == "none") {
-                            $(this).parent().removeClass("active");
-                        } else {
-                            $(this).parent().addClass("active")
-                        }
-                    });
-                }
-            });
-
+            $(".collapse-head-om")
+                .not(this)
+                .parent()
+                .find(".list-collapse-om")
+                .slideUp();
+            $(this)
+                .parent()
+                .find(".list-collapse-om")
+                .slideToggle({
+                    queue: false,
+                    complete: function () {
+                        $(".list-collapse-om").each(function () {
+                            if ($(this).css("display") == "none") {
+                                $(this).parent().removeClass("active");
+                            } else {
+                                $(this).parent().addClass("active");
+                            }
+                        });
+                    },
+                });
         });
     }
 
-
     /********************************
-     * 
+     *
      * menu active and close button  *
-     * 
-    ********************************/
-    // nav men activation 
+     *
+     ********************************/
+    // nav men activation
     $("#menu-butt-activ-om").on("click", function (e) {
         e.preventDefault();
 
@@ -210,58 +203,71 @@ jQuery(document).ready(function ($) {
         $("body").addClass("overflow-body");
     });
 
-    // nav men close 
+    // nav men close
     $(".close-butt-om , .overlay ").on("click", function (e) {
         e.preventDefault();
         $("#navbar-menu-om").removeClass("active-menu");
         $(".overlay").removeClass("active");
 
         $("body").removeClass("overflow-body");
-
     });
 
-
     /********************************
-     * 
+     *
      * sticky navbar *
-     * 
+     *
      ********************************/
-    // asign height to the header 
+    // asign height to the header
     var headerHeight = $("header").outerHeight();
     // $("header").height(headerHeight);
 
     var lastScroll = 0;
-    $(document).on('scroll', function () {
+    $(document).on("scroll", function () {
         var currentScroll = $(this).scrollTop();
 
         // scroll down
         if (currentScroll < lastScroll && currentScroll > headerHeight + 100) {
-            // add class avtive menu 
+            // add class avtive menu
             // if ($(".fixed-header-warper").hasClass("not-active-menu-om")) {
             $(".fixed-header-warper").addClass("active-menu-om");
             $(".fixed-header-warper").removeClass("not-active-menu-om");
             // }
             // console.log("move up");
-
-        } else if (currentScroll > lastScroll && currentScroll > headerHeight + 100) { // scroll up 
-            // remove class avtive menu 
+        } else if (
+            currentScroll > lastScroll &&
+            currentScroll > headerHeight + 100
+        ) {
+            // scroll up
+            // remove class avtive menu
             if ($(".fixed-header-warper").hasClass("active-menu-om")) {
-
                 $(".fixed-header-warper").removeClass("active-menu-om");
                 $(".fixed-header-warper").addClass("not-active-menu-om");
             }
             // $("#search-button-activation-om").removeClass("search-is-active");
             // $("#search-form-act-om").addClass("not-active").removeClass("active");
-
         } else {
             $(".fixed-header-warper").removeClass("active-menu-om");
             $(".fixed-header-warper").removeClass("not-active-menu-om");
         }
         lastScroll = currentScroll;
-
     });
 
+    $(".input_file__ ").on("change", function (e) {
+        let fileName = " ";
+        let elementToTakeFileVal = $(this)
+            .closest(".parent___group__")
+            .find(".file_name__");
 
-lazyLoad();
+        if (e.target.files[0]) {
+            fileName = e.target.files[0].name;
+            elementToTakeFileVal.addClass("active")
+        } else {
+            elementToTakeFileVal.removeClass("active")
+        }
 
+
+        elementToTakeFileVal.text(fileName);
+    });
+
+    lazyLoad();
 });
